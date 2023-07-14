ad5f608515d6f43df75bd8c7421fb4a9ffe1a3f4
Yume <hei.hajatiana@gmail.com>
2023-07-14 11:27:04 +0300

    feat: initialize peer server

---
+++ app.js
@@ -0,0 +1,17 @@
+const { PeerServer } = require('peer');
+
+require('dotenv').config();
+
+const endpoint = process.env.ENDPOINT ?? '/peer';
+const port = process.env.PORT ?? 8092;
+
+const server = new PeerServer({
+  path: endpoint,
+  port
+});
+
+function onStartServer() {
+  console.log(`Peer server running at http://localhost:${port}${endpoint}`);
+}
+
+server.listen(onStartServer);
